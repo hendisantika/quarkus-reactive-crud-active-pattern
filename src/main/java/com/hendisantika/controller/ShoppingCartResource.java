c08840d14ab7e31852a5aeca221462d6495103c0
hendisantika <hendisantika@yahoo.co.id>
2023-12-17 20:16:40 +0700

    Add getCarts in ShoppingCartResource🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/controller/ShoppingCartResource.java
+++ src/main/java/com/hendisantika/controller/ShoppingCartResource.java
@@ -1,6 +1,12 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.ShoppingCart;
+import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +20,11 @@ import jakarta.ws.rs.Path;
  */
 @Path("/v1/carts")
 public class ShoppingCartResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<Response> getCarts() {
+        return ShoppingCart.getAllShoppingCarts()
+                .onItem().transform(shoppingcarts -> Response.ok(shoppingcarts))
+                .onItem().transform(Response.ResponseBuilder::build);
+    }
 }

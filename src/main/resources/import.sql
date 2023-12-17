4afe30dd9b9ea281ed0637351813e98de16c7f50
hendisantika <hendisantika@yahoo.co.id>
2023-12-17 20:01:32 +0700

    Add import.sql file🫸🌀✏️📗🐧🐳

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO product(title, description, createdat, updatedat)
+VALUES ('Product1', 'description', now(), now());
+INSERT INTO product(title, description, createdat, updatedat)
+VALUES ('Product2', 'description', now(), now());
+INSERT INTO product(title, description, createdat, updatedat)
+VALUES ('Product3', 'description', now(), now());
+INSERT INTO product(title, description, createdat, updatedat)
+VALUES ('Product4', 'description', now(), now());
+INSERT INTO shoppingcart(carttotal, name)
+VALUES (0, 'MyCart');

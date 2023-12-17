c7d83f77d3a96cc71a398a0aa6d4cb5bb1edd9a3
hendisantika <hendisantika@yahoo.co.id>
2023-12-17 20:16:57 +0700

    Add getSingleCart in ShoppingCartResource🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/controller/ShoppingCartResource.java
+++ src/main/java/com/hendisantika/controller/ShoppingCartResource.java
@@ -4,9 +4,12 @@ import com.hendisantika.entity.ShoppingCart;
 import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +29,15 @@ public class ShoppingCartResource {
         return ShoppingCart.getAllShoppingCarts()
                 .onItem().transform(shoppingcarts -> Response.ok(shoppingcarts))
                 .onItem().transform(Response.ResponseBuilder::build);
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<Response> getSingleCart(@PathParam("id") Long id) {
+        return ShoppingCart.findByShoppingCartId(id)
+                .onItem().ifNotNull().transform(cart -> Response.ok(cart).build())
+                .onItem().ifNull().continueWith(Response.ok().status(NOT_FOUND)::build);
+
     }
 }

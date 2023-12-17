adfb8c7d7adba199f4616e3a5fe650f61023707c
hendisantika <hendisantika@yahoo.co.id>
2023-12-17 20:13:59 +0700

    Add ProductResource🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/controller/ProductResource.java
@@ -0,0 +1,17 @@
+package com.hendisantika.controller;
+
+import jakarta.ws.rs.Path;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-reactive-crud-active-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/17/23
+ * Time: 20:13
+ * To change this template use File | Settings | File Templates.
+ */
+@Path("/v1/products")
+public class ProductResource {
+}

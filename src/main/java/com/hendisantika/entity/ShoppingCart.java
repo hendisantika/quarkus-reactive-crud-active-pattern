e3777f748273131b14c553ea1ea2afe05e3e8ae2
hendisantika <hendisantika@yahoo.co.id>
2023-12-17 20:04:26 +0700

    Add ShoppingCart Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/entity/ShoppingCart.java
@@ -0,0 +1,43 @@
+package com.hendisantika.entity;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-reactive-crud-active-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/17/23
+ * Time: 20:04
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@NamedQueries(value = {@NamedQuery(name = "ShoppingCart.findAll",
+        query = "SELECT c FROM ShoppingCart c LEFT JOIN FETCH c.cartItems item LEFT JOIN FETCH item.product"),
+        @NamedQuery(name = "ShoppingCart.getById",
+                query = "SELECT c FROM ShoppingCart c LEFT JOIN FETCH c.cartItems item LEFT JOIN FETCH item.product where c.id = ?1")})
+public class ShoppingCart extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public int cartTotal;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    @JoinColumn
+    public Set<ShoppingCartItem> cartItems;
+
+    public String name;
+}

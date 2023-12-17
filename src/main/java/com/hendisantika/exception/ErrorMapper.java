27a7405cdb5e1dcfa0a3525ff1953dab74300ea0
hendisantika <hendisantika@yahoo.co.id>
2023-12-17 20:12:53 +0700

    Add toResponse in ErrorMapper🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/exception/ErrorMapper.java
+++ src/main/java/com/hendisantika/exception/ErrorMapper.java
@@ -1,8 +1,14 @@
 package com.hendisantika.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.smallrye.mutiny.CompositeException;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import org.jboss.logging.Logger;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,5 +26,34 @@ public class ErrorMapper implements ExceptionMapper<Exception> {
     private static final Logger LOGGER = Logger.getLogger(ErrorMapper.class.getName());
 
     @Inject
-    ObjectMapper objectMapper;
+    private ObjectMapper objectMapper;
+
+    @Override
+    public Response toResponse(Exception exception) {
+        LOGGER.error("Failed to handle request", exception);
+
+        Throwable throwable = exception;
+
+        int code = 500;
+        if (throwable instanceof WebApplicationException) {
+            code = ((WebApplicationException) exception).getResponse().getStatus();
+        }
+
+        // This is a Mutiny exception and it happens
+        if (throwable instanceof CompositeException) {
+            throwable = throwable.getCause();
+        }
+
+        ObjectNode exceptionJson = objectMapper.createObjectNode();
+        exceptionJson.put("exceptionType", throwable.getClass().getName());
+        exceptionJson.put("code", code);
+
+        if (exception.getMessage() != null) {
+            exceptionJson.put("error", throwable.getMessage());
+        }
+
+        return Response.status(code)
+                .entity(exceptionJson.toPrettyString())
+                .build();
+    }
 }

8f124167302378710d71bb6d33f0f47716ff9288
hendisantika <hendisantika@yahoo.co.id>
2023-12-17 20:14:37 +0700

    Add getSingleProduct in ProductResource🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/controller/ProductResource.java
+++ src/main/java/com/hendisantika/controller/ProductResource.java
@@ -4,9 +4,12 @@ import com.hendisantika.entity.Product;
 import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +29,13 @@ public class ProductResource {
         return Product.getAllProducts()
                 .onItem().transform(products -> Response.ok(products))
                 .onItem().transform(Response.ResponseBuilder::build);
+    }
+
+    @GET
+    @Path("{id}")
+    public Uni<Response> getSingleProduct(@PathParam("id") Long id) {
+        return Product.findByProductId(id)
+                .onItem().ifNotNull().transform(product -> Response.ok(product).build())
+                .onItem().ifNull().continueWith(Response.ok().status(NOT_FOUND)::build);
     }
 }
